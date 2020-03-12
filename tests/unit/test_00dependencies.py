abbd7098f137f58d9446339322b9f8c427263051
Greg Lindahl <glindahl@cfa.harvard.edu>
2020-03-12 14:30:27 -0400

    another sniff test

--- tests/unit/test_00dependencies.py
+++ tests/unit/test_00dependencies.py
@@ -1,5 +1,13 @@
+import os
+import os.path
+
 def test_pygrib_import():
     try:
         import pygrib
     except ImportError as e:
         raise ValueError('pygrib import failed, you need to install it but not using pip: '+str(e))
+
+
+def test_am_environment():
+    assert 'AM' in os.environ, 'AM environment variable is set'
+    assert os.path.isfile(os.environ['AM']), 'AM environment variable points at a file'

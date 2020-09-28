6086e395152c0f9deb92d4ab04c5ab112337ac07
Greg Lindahl <lindahl@pbm.com>
2020-09-28 10:47:06 -0700

    fix timezone computations in test

--- tests/unit/test_gfs_dates.py
+++ tests/unit/test_gfs_dates.py
@@ -3,8 +3,9 @@ import eht_met_forecast
 
 
 def test_latest_gfs_cycle_time():
-    t = eht_met_forecast.latest_gfs_cycle_time(now=1234567890., lag=5.2)
-    print('hint: test time is', datetime.datetime.fromtimestamp(1234567890.).strftime('%Y%m%d%H'))
-    assert t.strftime('%Y%m%d%H') == '2009021306'
-    t = eht_met_forecast.latest_gfs_cycle_time(now=1234567890.)
+    print('hint: test time is', datetime.datetime.fromtimestamp(1234567890., tz=datetime.timezone.utc).strftime('%Y%m%d%H'))
+
+    t = eht_met_forecast.latest_gfs_cycle_time(now=1234564890., lag=5.2)
     assert t.strftime('%Y%m%d%H') == '2009021312'
+    t = eht_met_forecast.latest_gfs_cycle_time(now=1234567890.)
+    assert t.strftime('%Y%m%d%H') == '2009021318'

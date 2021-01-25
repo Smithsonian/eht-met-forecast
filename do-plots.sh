d103906b37ce854dbcc61d534fd794450fa7d0b5
Greg Lindahl <lindahl@pbm.com>
2021-01-25 09:55:50 -0800

    make webpages

---
+++ do-plots.sh
@@ -0,0 +1,12 @@
+python scripts/scott.py --verbose --outputdir ./eht-met-plots/ 384
+python scripts/scott.py --verbose --outputdir ./eht-met-plots/ 120
+# lindy plot
+
+python scripts/make-jumbo-webpage.py
+
+rm -f eht-met-plots latest
+(cd eht-met-plots/ && ln -s `ls | tail -n 1` latest)
+
+rsync -av eht-met-plots/ glindahl@35.199.60.65:public_html/eht-met-plots/
+
+# hint: https://wiki.ehtcc.org/~glindahl/eht-met-plots/latest

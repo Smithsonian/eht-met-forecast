7dee2a96d418d11e2cf491ce3cbed236a1495c99
Greg Lindahl <lindahl@pbm.com>
2021-03-23 16:42:08 +0000

    raise python version floor for reasons

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author_email='glindahl@cfa.harvard.edu',
     url='https://github.com/wumpus/eht-met-forecast',
     packages=packages,
-    python_requires=">=3.4.*",
+    python_requires=">=3.6.*",
     extras_require=extras_require,
     include_package_data=True,
     package_data=package_data,
@@ -57,7 +57,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
+        #'Programming Language :: Python :: 3.5',  # setuptools_scm now has f-strings
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',

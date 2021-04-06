2a0844474a59318682b5cd0d74b937801992618f
Greg Lindahl <lindahl@pbm.com>
2021-04-06 12:48:28 -0700

    optional wait for downloads

--- do-all.sh
+++ do-all.sh
@@ -4,15 +4,19 @@ DEST=~/github/eht-met-data
 
 $AM -v || exit 1
 
-TIME=192
+BACKFILL=24
+
+#WAIT="--wait"
+#WAIT=""
 
 STATIONS="Aa Ax BAJA BOL GAM Gl HAY Kt LAS Lm Mg Mm Nn OVRO PIKES Pv Sw Sz VLA VLT"
 
 for vex in $STATIONS; do
-   eht-met-forecast --backfill $TIME --dir $DEST --vex $vex &
+   eht-met-forecast --backfill $BACKFILL --dir $DEST --vex $vex $WAIT &
 done
 
 wait
 
 (cd $DEST && bash ./commit-finished.sh)
 
+ssh glindahl@35.199.60.65 "cd eht-met-data && git pull"

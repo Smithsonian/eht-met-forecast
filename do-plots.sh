341ce4c7e65e816e521f346a7082b67145abb7ea
Greg Lindahl <lindahl@pbm.com>
2021-04-12 04:29:42 +0000

    use a separate repo with just whole data files

--- do-plots.sh
+++ do-plots.sh
@@ -1,5 +1,7 @@
-python scripts/scott.py --verbose --plotdir ./eht-met-plots/ 384 &
-python scripts/scott.py --verbose --plotdir ./eht-met-plots/ 120 &
+(cd ~/github/eht-met-data-prod && git pull)
+
+python scripts/scott.py --verbose --plotdir ./eht-met-plots/ --datadir ~/github/eht-met-data-prod 384 &
+python scripts/scott.py --verbose --plotdir ./eht-met-plots/ --datadir ~/github/eht-met-data-prod 120 &
 
 # also change in do-deploy.sh
 EHT2021="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz"
@@ -7,7 +9,7 @@ EHT2021="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz"
 rm -f tau225.txt
 wget https://vlbimon1.science.ru.nl/img/plots/tau225.txt
 
-python scripts/lindy.py --vex e21[abcde]*.vex e2_[f].vex --emphasize $EHT2021 --start 2021:04:09 --end 2021:04:20 &
+python scripts/lindy.py --vex e21[abcde]*.vex e2_[f].vex --emphasize $EHT2021 --start 2021:04:09 --end 2021:04:20 --datadir ~/github/eht-met-data-prod &
 
 wait
 

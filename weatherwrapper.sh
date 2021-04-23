36ce3f620f11ca8b1b57986884dfd20211f59e02
Greg Lindahl <lindahl@pbm.com>
2021-04-23 13:27:48 -0700

    track how long things take to run

--- weatherwrapper.sh
+++ weatherwrapper.sh
@@ -6,11 +6,16 @@ export AM=./am-11.0/src/am
 
 cd ~/github/eht-met-forecast
 
+date
+
 # first download anything in the past
 bash do-all.sh
+
+date
 
 # now wait for the next one
 # turn this one when we're running
 #WAIT=--wait bash do-all.sh
 
+date
 

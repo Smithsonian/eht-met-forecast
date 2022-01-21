4bbb5541cb3928f384e3ef78065f5685f573a401
Greg Lindahl <lindahl@pbm.com>
2022-01-21 14:41:16 -0800

    2022 DR config

--- do-deploy.sh
+++ do-deploy.sh
@@ -1,7 +1,8 @@
 # also change in do-plots.sh
-EHT2021="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz"
+#EHT2021="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz"
+EHT2022DR="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm"
 
-python scripts/make-jumbo-webpage.py --emphasize $EHT2021
+python scripts/make-jumbo-webpage.py --emphasize $EHT2022DR
 
 rm -f eht-met-plots/latest
 LATEST_TIME=`cd eht-met-plots/ && ls -d 202* | tail -n 1`
@@ -11,8 +12,11 @@ rm -f eht-met-plots/latest/lindy_00.png
 rm -f eht-met-plots/latest/lindy_00e.png
 (cd eht-met-plots/latest/ && ln -s lindy_00e_$LATEST_TIME.png lindy_00e.png)
 
+# this is eht-work ... the homedirs are mounted using NFS so this rsync can be
+# done to any eht cloud server
 rsync -av eht-met-plots/ glindahl@35.199.60.65:public_html/eht-met-plots/
 
+# Lindy sometimes wants to access this data, so keep it up-to-date
 ssh glindahl@35.199.60.65 "cd eht-met-data && git pull"
 
 # hint: https://wiki.ehtcc.org/~glindahl/eht-met-plots/latest
--- do-plots.sh
+++ do-plots.sh
@@ -1,15 +1,23 @@
-(cd ~/github/eht-met-data-prod && git pull)
+# this data directory should be the checked in data, with no partial files
+DATA=~/github/eht-met-data-prod
 
-python scripts/scott.py --verbose --plotdir ./eht-met-plots/ --datadir ~/github/eht-met-data-prod 384 &
-python scripts/scott.py --verbose --plotdir ./eht-met-plots/ --datadir ~/github/eht-met-data-prod 120 &
+(cd $DATA && git pull)
+
+python scripts/scott.py --verbose --plotdir ./eht-met-plots/ --datadir $DATA 384 &
+python scripts/scott.py --verbose --plotdir ./eht-met-plots/ --datadir $DATA 120 &
 
 # also change in do-deploy.sh
-EHT2021="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz"
+#EHT2021="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz"
+EHT2022DR="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm"
+START=2022:01:25  # in UT. in EST and PST the start is 24... looking forward to confusion!
+END=2022:01:28
+VEX=dr2022-schedules/e22*.vex
 
+# XXX look at the first line to rename this file by date
 rm -f tau225.txt
 wget https://vlbimon1.science.ru.nl/img/plots/tau225.txt
 
-python scripts/lindy.py --vex e21[abcde]*.vex e21f19.vex --emphasize $EHT2021 --start 2021:04:09 --end 2021:04:20 --datadir ~/github/eht-met-data-prod &
+python scripts/lindy.py --vex $VEX --emphasize $EHT2022DR --start $START --end $END --datadir $DATA &
 
 wait
 

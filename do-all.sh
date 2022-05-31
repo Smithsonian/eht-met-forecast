19783a92da5e867efa516fabed086db1e07c1479
Greg Lindahl <lindahl@pbm.com>
2022-05-30 21:44:06 -0700

    better crash for misconfiguration

--- do-all.sh
+++ do-all.sh
@@ -4,9 +4,6 @@ STATIONS="Aa Ax BAJA BOL GAM Gl HAY Kt LAS Lm Mg Mm Nn OVRO PIKES Pv Sw Sz VLA V
 DEST=~/github/eht-met-data
 UPLOAD=glindahl@35.199.60.65
 
-# die early if this isn't present
-$AM -v || exit 1
-
 # better set lower during an observation
 # if too large, the oldest ones see http 302 (redir)
 BACKFILL=168
--- weatherwrapper.sh
+++ weatherwrapper.sh
@@ -2,15 +2,22 @@
 # it is important for the above line to be bash and not sh
 
 . ~/venv/eht-met-forecast/bin/activate
-export AM=./am-12.0/src/am
-
 cd ~/github/eht-met-forecast
+
+# die early if this isn't present or somehow won't start
+export AM=./am-12.0/src/am
+# -bash: -v: command not found
+$AM -v > /dev/null || exit 1
 
 date -u
 
+# this will do nothing if the past was already downloaded with wait
 echo downloading in the past
 
 bash do-all.sh
+#echo doing plots, if any
+#bash do-plots.sh
+#bash do-deploy.sh
 
 date -u
 
@@ -20,6 +27,7 @@ date -u
 
 #echo doing plots
 #bash do-plots.sh
+#bash do-deploy.sh
 
 # now we'd like to notify Greg and/or slack, BUT, only for the run starting at 01 UT
 

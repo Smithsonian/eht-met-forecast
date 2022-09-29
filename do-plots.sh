9332d1191cc78f2e9eda1700cdafd6477e1e4a65
Greg Lindahl <lindahl@pbm.com>
2022-09-28 21:42:25 -0700

    eht2022

--- do-plots.sh
+++ do-plots.sh
@@ -10,10 +10,11 @@ python scripts/scott.py --verbose --plotdir ./eht-met-plots/ --datadir $DATA 120
 
 # also change in do-deploy.sh
 #EHT2021="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz"
-EHT2022DR="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm"
-START=2022:01:25  # in UT. in EST and PST the start is 24... looking forward to confusion!
-END=2022:01:28
-VEX=dr2022-schedules/e22*.vex
+EHT2022="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm"
+START=2022:03:17  # in UT. It's the 16th in EDT.
+#START=2022:03:11  # in UT. It's the 10th in EDT. Special weather for Alma fringe test.
+END=2022:03:28
+VEX=2022-schedules/trak?.vex
 
 # XXX look at the first line to rename this file by date
 rm -f tau225.txt
@@ -21,7 +22,7 @@ wget https://vlbimon1.science.ru.nl/img/plots/tau225.txt
 # wget will prevent overwrites, giving us a history
 #wget https://vlbimon1.science.ru.nl/img/plots/tau225.txt.archive
 
-python scripts/lindy.py --vex $VEX --emphasize $EHT2022DR --start $START --end $END --datadir $DATA &
+python scripts/lindy.py --vex $VEX --emphasize $EHT2022 --start $START --end $END --datadir $DATA &
 
 wait
 date -u

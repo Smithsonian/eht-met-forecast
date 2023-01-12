e004853661626dde5b0ca05592fda642a9cbfae2
Greg Lindahl <lindahl@pbm.com>
2023-01-12 14:22:55 -0800

    slack progress bar

--- weatherwrapper.sh
+++ weatherwrapper.sh
@@ -9,24 +9,35 @@ export AM=./am-12.2/src/am
 # -bash: -v: command not found
 $AM -v > /dev/null || exit 1
 
+export GFS=$(python scripts/print-gfs-cycle.py)
+
 date -u
 
 # this will do nothing if the past was already downloaded with wait
 echo downloading in the past
 
 bash do-all.sh
-#echo doing plots, if any
-#bash do-plots.sh
-#bash do-deploy.sh
+echo doing plots, if any
+bash do-plots.sh
+bash do-deploy.sh
 
 date -u
 
-# turn this on when we're running
-#echo downloading latest with wait
-#WAIT=--wait bash do-all.sh
+# the rest of the script is for when we are observing
 
 if echo $GFS | grep -q 12:00:00; then
-  python scripts/slack-post eht infra_bots "12UT weather downloaded, charts in another 20 minutes"
+  WATCHFILE=$DEST/Aa/$GFS
+  python scripts/download-watcher.py $WATCHFILE &
+  jobs -l
+  disown -a
+  jobs -l
+fi
+
+echo downloading latest with wait
+FLUSH="--flush Aa" WAIT=--wait bash do-all.sh
+
+if echo $GFS | grep -q 12:00:00; then
+  python scripts/slack-post.py eht infra_bots "12UT weather downloaded, charts in another 20 minutes"
 fi
 
 echo doing plots
@@ -36,10 +47,10 @@ bash do-deploy.sh
 # now we'd like to notify Greg and/or slack, BUT, only for the run starting at 13 UT
 
 if echo $GFS | grep -q 06:00:00; then
-  python scripts/slack-post eht infra_bots "06UT weather charts are available"
+  python scripts/slack-post.py eht infra_bots "06UT weather charts are available"
 fi
 if echo $GFS | grep -q 12:00:00; then
-  python scripts/slack-post eht infra_bots "12UT weather charts are available"
+  python scripts/slack-post.py eht infra_bots "12UT weather charts are available"
 fi
 
 date -u

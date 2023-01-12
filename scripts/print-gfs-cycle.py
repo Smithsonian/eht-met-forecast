9402356f78d9417510fc734c217905e9858164d0
Greg Lindahl <lindahl@pbm.com>
2023-01-11 18:47:58 -0800

    bug

--- scripts/print-gfs-cycle.py
+++ scripts/print-gfs-cycle.py
@@ -1,4 +1,6 @@
 from eht_met_forecast.gfs import latest_gfs_cycle_time
 
+gfs = latest_gfs_cycle_time()
 
-print(latest_gfs_cycle_time())
+GFS_TIMESTAMP = '%Y%m%d_%H:00:00'
+print(gfs.strftime(GFS_TIMESTAMP))

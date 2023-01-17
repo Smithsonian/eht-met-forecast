380902209a1a6a370c0d53848df2ed7905a14df6
Greg Lindahl <lindahl@pbm.com>
2023-01-16 18:32:59 -0800

    warning and error alert

--- scripts/download-watcher.py
+++ scripts/download-watcher.py
@@ -12,16 +12,24 @@ if os.getenv('SLACK_QUIET'):
     exit(0)
 
 token, channel_id = slack_utils.get_slack_token('eht', 'ehtobs_bots')
+webhook = slack_utils.get_slack_webhook('eht', 'ehtobs_bots')
 
-name = sys.argv[1]
+fname = sys.argv[1]
 
-# wait up to 2 hours for this file to appear
+# wait up to 4 hours for this file to appear
+warned = False
 start = time.time()
-while time.time() < start + 7200:
-    if os.path.exists(name):
+while time.time() < start + 14400:
+    if os.path.exists(fname):
         break
+    # but warn after 2 hours:
+    if not warned and time.time() > start + 7200:
+        slack_utils.slack_message('Warning: GFS download has not yet started, still trying', webhook)
+        warned = True
+else:
+    slack_utils.slack_message('Error: GFS download has not yet started, giving up', webhook)
 
-with open(name) as fd:
+with open(fname) as fd:
     count = len(fd.read().splitlines())
 
 tqdm_bar = trange(210, initial=count, token=token, channel=channel_id)
@@ -30,7 +38,7 @@ tqdm_bar.set_description('GFS download')
 last = count
 
 while True:
-    with open(name) as fd:
+    with open(fname) as fd:
         count = len(fd.read().splitlines())
     if count != last:
         tqdm_bar.update(count - last)

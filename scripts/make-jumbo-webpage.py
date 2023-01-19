de0d068e9a88ddf13b94b80ba14241910c70de11
Greg Lindahl <lindahl@pbm.com>
2023-01-19 11:21:44 -0800

    refactor

--- scripts/make-jumbo-webpage.py
+++ scripts/make-jumbo-webpage.py
@@ -11,42 +11,39 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from eht_met_forecast import read_stations
 
-# argparse
-# --force,-f
-# --emphasize
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--stations', action='store', help='location of stations.json file')
 parser.add_argument('--emphasize', action='store', nargs='+', help='colon-delimited list of stations to emphasize, i.e. this year\'s array')
 parser.add_argument('--plotdir', action='store', default='./eht-met-plots', help='output directory for plots')
-parser.add_argument('--force', action='store_true', help='make the plot even if the output file already exists')
+parser.add_argument('--force', action='store_true', help='generate index.html even if it already exists')
 
 args = parser.parse_args()
 plotdir = os.path.expanduser(args.plotdir)
 
 emphasize = set(station for station in args.emphasize if ':' not in station)
 [emphasize.add(s) for station in args.emphasize if ':' in station for s in station.split(':')]
-if '00' not in emphasize:
-    emphasize.add('00')
-if '00e' not in emphasize:
-    emphasize.add('00e')
-if '00w' not in emphasize:
-    emphasize.add('00w')
-if '00wg' not in emphasize:
-    emphasize.add('00wg')
-if '00p' not in emphasize:
-    emphasize.add('00p')
+
+not_stations = {
+    '00': 'Current stations GFS weather',
+    '00e': 'Current stations EU weather',
+    '00w': 'Current stations GFS 10m wind',
+    '00wg': 'Current stations GFS wind gusts > 10 m/s',
+    '00p': 'Current stations GFS precip chance',
+    '01': 'Future stations',
+}
+
+for ns in not_stations.keys():
+    if ns == '01':
+        continue
+    if ns not in emphasize:
+        emphasize.add(ns)
 
 stations = read_stations(args.stations)
-stations['00'] = {'name': 'Current stations GFS weather'}
-stations['00e'] = {'name': 'Current stations EU weather'}
-stations['00w'] = {'name': 'Current stations GFS 10m wind'}
-stations['00wg'] = {'name': 'Current stations GFS wind gusts > 10 m/s'}
-stations['00p'] = {'name': 'Current stations GFS precip chance -- new for 2023'}
-stations['01'] = {'name': 'Future stations'}
+for ns, name in not_stations.items():
+    stations[ns] = {'name': name}
 
 for e in emphasize:
-    if e not in stations and e not in {'00', '00e', '00w', '00wg', '00p'}:
+    if e not in stations and e not in not_stations:
         raise ValueError('emphasized station {} is not known'.format(e))
 
 env = Environment(
@@ -55,8 +52,9 @@ env = Environment(
     autoescape=select_autoescape(['html'])
 )
 
-# XXX
-dirs = glob.glob(args.plotdir + '/2*')
+dirs = glob.glob(args.plotdir + '/202[3456789]*')
+
+symlinks = set(['lindy_{}.png'.format(ns) for ns in not_stations.keys()])
 
 for d in dirs:
     if os.path.exists(d+'/index.html') and not args.force:
@@ -71,20 +69,7 @@ for d in dirs:
         if f.endswith('.csv'):
             continue
         f = f.split('/')[-1]
-        if f == 'lindy_00.png':
-            # a symlink
-            continue
-        if f == 'lindy_00e.png':
-            # a symlink
-            continue
-        if f == 'lindy_00w.png':
-            # a symlink
-            continue
-        if f == 'lindy_00wg.png':
-            # a symlink
-            continue
-        if f == 'lindy_00p.png':
-            # a symlink
+        if f in symlinks:
             continue
         parts = f.split('_')
         if parts[0] in prefixes:
@@ -95,29 +80,22 @@ for d in dirs:
 
     for s in sorted(groups.keys()):
         if s not in stations:
-            # renamed things
-            print('I had files for station {} but it is not in stations'.format(s))
+            # renamed stations end up here
+            print('I found files for station {} but it is not in stations'.format(s))
             continue
-        if emphasize:
-            if s in emphasize:
-                now[s] = groups[s]
-            else:
-                future[s] = groups[s]
+        if s in emphasize:
+            now[s] = groups[s]
         else:
-            if len(s) != 2:
-                future[s] = groups[s]
-            else:
-                now[s] = groups[s]
+            future[s] = groups[s]
 
-    stuff = {}
-    stuff['gfs_cycle'] = gfs_cycle
-    stuff['title'] = '{} Plots'.format(gfs_cycle)
-    stuff['year'] = gfs_cycle[:4]
-    stuff['stations'] = stations
-
-    # XXX need to read trackrank.csv to compute these
-    stuff['trackmin'] = 0.55
-    stuff['trackmax'] = 0.75
+    stuff = {
+        'gfs_cycle': gfs_cycle,
+        'title': '{} Plots'.format(gfs_cycle),
+        'year': gfs_cycle[:4],
+        'stations': stations,
+        'trackmin': 0.55,  # used to color the trackrank csv
+        'trackmax': 0.75,
+    }
 
     template = env.get_template('index.html.template')
     with open(d + '/index.html', 'w') as f:

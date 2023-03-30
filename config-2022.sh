9ffa6c9f5010913b0644eedc289de7356bb47cac
Greg Lindahl <lindahl@pbm.com>
2023-03-29 22:01:14 -0700

    previous year configs

---
+++ config-2022.sh
@@ -0,0 +1,8 @@
+export STATIONS="Aa Ax BAJA BOL BRZ CAT CNI GAM Gl GLTS HAY Kt LAS Lm Mg Mm Nn OVRO PIKES Pv Sw Sz VLA VLT"
+export START=2022:03:17  # in UT. It's the 16th in EDT.
+export END=2022:03:28
+export VEX=2022/*.vex
+export EMPHASIZE="Nn:Pv:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm"
+
+export DEST=~/github/eht-met-data
+export UPLOAD=glindahl@35.199.60.65
---
+++ config-2023dr.sh
@@ -0,0 +1,8 @@
+export STATIONS="Aa Ax BAJA BOL BRZ CAT CNI GAM Gl GLTS HAY Kt LAS Lm Mg Mm Nn OVRO PIKES Pv Sw Sz VLA VLT"
+export START=2023:01:16  # in UT.
+export END=2023:01:19
+export VEX=dr2023-schedules/*.vex
+export EMPHASIZE="Nn:Gl:Aa:Sw:Mm:Lm"
+
+export DEST=~/github/eht-met-data
+export UPLOAD=glindahl@35.199.60.65

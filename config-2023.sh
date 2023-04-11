a00583c4cac6c2ae4189baeb7b62310d50cbadf6
Greg Lindahl <lindahl@pbm.com>
2023-04-11 11:31:13 -0700

    I diligently copied Remo's typo

--- config-2023.sh
+++ config-2023.sh
@@ -1,6 +1,6 @@
 export STATIONS="Aa Ax BAJA BOL BRZ CAT CNI GAM Gl GLTS HAY Kt LAS Lm Mg Mm Nn OVRO PIKES Pv Sw Sz VLA VLT"
 export START=2023:04:12  # in UT. traka starts at 2200 ut 11
-export END=2023:04:22
+export END=2023:04:23
 export VEX=2023-schedules/*.vex
 export EMPHASIZE="Nn:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm"
 

ab32f9e67991c013f24d0f8465ab231ff43a1c30
Greg Lindahl <lindahl@pbm.com>
2023-11-19 14:36:37 -0800

    cron tweaks

--- eurodownload.sh
+++ eurodownload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # it is important for the above line to be bash and not sh
 
+set -e
+
 . ~/venv/eht-met-forecast/bin/activate
 cd ~/github/eht-met-forecast
 
@@ -10,5 +12,9 @@ RETRIES="--waitretry=1m -t 60"  # linear backoff, this is a total of 30 minutes
 #RETRY_THINGS="--retry-connrefused --retry-on-http-error=404 --retry-on-http-error=500 --retry-on-http-error=502"  # needs more recent wget than I have
 RETRY_THINGS="--retry-connrefused"
 wget $RETRY_THINGS $RETRIES https://vlbimon1.science.ru.nl/img/plots/tau225.txt > /dev/null
+
+if [ ! -f tau225.txt ]; then
+  wget $RETRY_THINGS $RETRIES https://vlbimon2.science.ru.nl/img/plots/tau225.txt > /dev/null
+fi
 
 python scripts/copy-to-timpstamped-name.py tau225.txt

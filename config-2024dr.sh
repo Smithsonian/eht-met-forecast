10c918efb611c589263e866594e5e98494f11994
Greg Lindahl <lindahl@pbm.com>
2024-01-15 16:41:01 -0800

    2024 dr

---
+++ config-2024dr.sh
@@ -0,0 +1,8 @@
+export STATIONS="Aa Ax BAJA BOL BRZ CAT CNI GAM Gl GLTS HAY Kt LAS Lm Mg Mm Nn OVRO PIKES Pv Sw Sz VLA VLT KVN-Pyeonchang"
+export START=2024:01:23  # in UT.
+export END=2024:01:25
+export VEX=dr2024-schedules/*.vex
+export EMPHASIZE="Nn:Gl:Aa:Sw:Mm:Lm"
+
+export DEST=~/github/eht-met-data
+export UPLOAD=glindahl@35.199.60.65

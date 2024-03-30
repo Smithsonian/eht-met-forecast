bad540bef17e33c53e77ec1ba4cd8b67d8ba034c
Greg Lindahl <lindahl@pbm.com>
2024-03-30 15:52:42 -0700

    missed one

--- config-2024.sh
+++ config-2024.sh
@@ -4,7 +4,7 @@ export STATIONS="Aa Ax BAJA CNI GAM Gl GLTS HAY JELM Kt LAS LLA Lm Mg Mm Nn OVRO
 export START=2024:04:04
 export END=2024:04:15
 export VEX="--vex 2024/*.vex"
-export EMPHASIZE="Nn:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm:Ky"
+export EMPHASIZE="Nn:Gl:Ax:Aa:Kt:Mg:Sw:Mm:Sz:Lm:Pv:Ky"
 
 export DEST=~/github/eht-met-data
 export DATA=~/github/eht-met-data-prod
